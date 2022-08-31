40dd982969fd14b8ecd8267a4b369bea395f56e8
SirPhemmiey <oluwafemiakinde@gmail.com>
2022-08-31 16:47:21 +0400

    add prediction output file and hadolint

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -14,9 +14,9 @@ kubectl create deploy project4 --image=sirphemmiey/project4
 # Step 3:
 # List kubernetes pods
 #kubectl get nodes
-kubectl get deploy,rs,svc,pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward pod/project4-6f66c6bd8d-7lvkn 8000:80
+kubectl port-forward project4-6f66c6bd8d-2z96b 8000:80
 
